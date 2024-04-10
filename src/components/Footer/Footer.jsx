4a871fc89902e423e461b37467fc4c13a04d7484
siddharth chauhan <siddharthchauhan2119@gmail.com>
2024-04-10 14:59:10 +0530

    add: initial files

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+            <img className="footer-logo" src={assets.logo} alt="" />
+            <p>Thank you for visiting Baratie. Also here is something my friend has to say to you</p>
+            
+            {/* <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div> */}
+        </div>
+        <div className="footer-content-center">
+            <h2>From my Friend</h2>
+            <ul>
+                <li>"You want my treasure, you can have it.</li>
+                <li>I left everything I gathered together in one place.</li>
+                <li>Now, you just have to find it."</li>
+                <li>  - Gol D. Roger.</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+            <div className="footer-social-icons">
+                <a href='https://www.linkedin.com/in/siddharth2119chauhan/' target='blank'><img src="/linkedin.png" alt="" width="20px"/></a>
+                <a href='mailto:siddharth2119chauhan@gmail.com'><img src="/google.png" alt="email" /></a>
+                <a><img src="/github.png" alt="gthub"  /></a>
+            </div>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Designed With💓 By Siddharth Chauhan</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Welcome to Restaurant Baratie!</h2>
+                <p>Inspired by the One Piece anime, Founded by the renowned chef "Red-Leg" Zeff and "Black-Foot" Sanji, we serve exceptional cuisine in a setting reminiscent of the high seas. , our restaurant offers a unique dining experience that's sure to delight fans and food enthusiasts alike.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
